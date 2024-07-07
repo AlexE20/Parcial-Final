e7fc8b4335a1228d8882c19a46b8a4331c3902d2
Andrea-004 <00106123@uca.edu.sv>
2024-07-07 02:33:17 -0600

    Adding comments to the Transaction

--- Parcial_Final/src/main/java/com/example/parcial_final/Transaction.java
+++ Parcial_Final/src/main/java/com/example/parcial_final/Transaction.java
@@ -2,37 +2,37 @@ package com.example.parcial_final;
 import java.time.LocalDate;
 
 public class Transaction {
-    private LocalDate purchaseDate;
-    private double amount;
-    private String description;
+    private LocalDate purchaseDate; //Declares purchaseDate attribute as LocalDate
+    private double amount; //Declares amount attribute as double
+    private String description; //Declares description attribute as String
 
     public Transaction(LocalDate purchaseDate, double amount, String description) {
-        this.purchaseDate = purchaseDate;
-        this.amount = amount;
-        this.description = description;
+        this.purchaseDate = purchaseDate; //Initializes purchaseDate
+        this.amount = amount; //Initializes amount
+        this.description = description; //Initializes description
     }
 
-    public LocalDate getPurchaseDate() {
+    public LocalDate getPurchaseDate() { //Getter for purchaseDate
         return purchaseDate;
     }
 
-    public void setPurchaseDate(LocalDate purchaseDate) {
+    public void setPurchaseDate(LocalDate purchaseDate) { //Setter for purchaseDate
         this.purchaseDate = purchaseDate;
     }
 
-    public double getAmount() {
+    public double getAmount() { //Getter for amount
         return amount;
     }
 
-    public void setAmount(double amount) {
+    public void setAmount(double amount) { //Setter for amount
         this.amount = amount;
     }
 
-    public String getDescription() {
+    public String getDescription() { //Getter for description
         return description;
     }
 
-    public void setDescription(String description) {
+    public void setDescription(String description) { //Setter for description
         this.description = description;
     }
 }
