5bc1e98298f04b1086e04f0039d963a08383767e
Pabloo-04 <00080323@uca.edu.sv>
2024-07-06 18:51:35 -0600

    Adding initial view, and handling multiple scenes.

---
+++ Parcial_Final/src/main/java/com/example/parcial_final/InitialScreenController.java
@@ -0,0 +1,68 @@
+package com.example.parcial_final;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class InitialScreenController {
+
+    private Parent root;//Clase base
+    private Stage stage;//Contenedor(ventana prncipal)
+    private Scene scene;//Escena del progama
+
+    @FXML
+    private Button btnReportA;//Boton para mostrar reporte A.
+
+    @FXML
+    private Button btnReportB;//Boton para mostrar reporte B.
+
+    @FXML
+    private Button btnReportC;//Boton para mostrar reporte C.
+
+    @FXML
+    private Button btnReportD;//Boton para mostrar reporte D.
+
+
+
+    @FXML
+    protected void onReportA_Click(ActionEvent event)throws IOException {//Método para cambiar a la escena del reporte A
+       // Parent root = FXMLLoader.load(getClass().getResource("reportC-view.fxml"));
+        stage =(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void onReportB_Click() {
+
+    }
+    @FXML
+    protected void onReportC_Click(ActionEvent event)throws IOException {
+        try{
+            root = FXMLLoader.load(getClass().getResource("reportC-view.fxml"));
+        }catch(NullPointerException e){
+            e.printStackTrace();
+        }
+        stage =(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
+    @FXML
+    protected void onReportD_Click() {
+
+    }
+
+
+}
---
+++ Parcial_Final/src/main/java/com/example/parcial_final/ReportC_Controller.java
@@ -0,0 +1,31 @@
+package com.example.parcial_final;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ReportC_Controller {
+
+    private Parent root;//Clase base
+    private Stage stage;//Contenedor(ventana prncipal)
+    private Scene scene;//Escena del progama
+
+    @FXML
+    protected void onReturnbtn_Click(ActionEvent event)throws IOException {//Método para cambiar a la escena del reporte A
+        try{
+            root = FXMLLoader.load(getClass().getResource("initial-view.fxml"));
+        }catch(NullPointerException e){
+            e.printStackTrace();
+        }
+        stage =(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
