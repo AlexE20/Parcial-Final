e16d5d427c56924d0f3359ea40c12c5fb622e103
JuanPayes <00005923@uca.edu.sv>
2024-07-08 23:14:36 -0600

    Adding delete methods, datePicker and a comboBox

--- Parcial_Final/src/main/java/com/example/parcial_final/InsertsController.java
+++ Parcial_Final/src/main/java/com/example/parcial_final/InsertsController.java
@@ -1,12 +1,11 @@
 package com.example.parcial_final; // Define el paquete del archivo 00009423
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent; // Importa clases para manejar eventos 00009423
 import javafx.fxml.FXML; // Importa anotaciones FXML  00009423
 import javafx.fxml.Initializable; // Importa la interfaz Initializable  00009423
-import javafx.scene.control.Alert; // Importa la clase Alert para diálogos  00009423
-import javafx.scene.control.Button; // Importa la clase Button  00009423
-import javafx.scene.control.ComboBox; // Importa la clase ComboBox  00009423
-import javafx.scene.control.TextField; // Importa la clase TextField  00009423
+import javafx.scene.control.*;
 
 import java.net.URL; // Importa la clase URL  00009423
 import java.sql.*; // Importa clases para manejo de SQL  00009423
@@ -20,7 +19,7 @@ public class InsertsController implements Initializable { // Define la clase pri
     @FXML
     private TextField txtCardType; // Define el campo de texto para el tipo de tarjeta  00009423
     @FXML
-    private ComboBox cmbFacilitator; // Define el ComboBox para el facilitador de la tarjeta  00009423
+    private ComboBox<Integer> cmbFacilitator; // Define el ComboBox para el facilitador de la tarjeta  00009423
     @FXML
     private TextField txtIdClient; // Define el campo de texto para el ID del cliente  00009423
     @FXML
@@ -59,10 +58,35 @@ public class InsertsController implements Initializable { // Define la clase pri
     private Button btnUpdateT; // Define el botón para actualizar una transacción  00009423
     @FXML
     private TextField txtIdTransaction; // Define el campo de texto para el ID de la transacción  00009423
+    @FXML
+    private DatePicker dpTransaction; //Define datePicker 00005923
+    @FXML
+    private DatePicker dpCard;  //Define datePicker for card expiration 00005923
+    @FXML
+    private TextField txtIdClientDel; //Define el textField que se ingresara el ID de client para borrarlo 00005923
+    @FXML
+    private TextField txtIdCardDel; //Define el textField que se ingresara el ID de Card para borrarlo 00005923
+    @FXML
+    private TextField txtIdTransactionDel; //Define el textField que se ingresara el ID de Transaction para borrarlo 00005923
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) { // Método de inicialización  00009423
+        cmbFacilitator.setItems(getFacilitatorIds()); //Inicializa los ids que estaran en el comboBox 00005923
+    }
 
+    private ObservableList<Integer> getFacilitatorIds() {
+        String query = "SELECT id_facilitator FROM facilitator"; //00005923 Es un SELECT que escoge los ids de la tabla Facilitador
+        ObservableList<Integer> facilitatorIds = FXCollections.observableArrayList(); //00005923 Es una lista que guardara dentro de una ObservableList
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbBank", "root", "Egualos123"); //00005923 Inicia la conexion con la Base de datos
+             Statement st = conn.createStatement(); //00005923 Crea un declaracion que sera usada para generar un resultado
+             ResultSet rs = st.executeQuery(query)) { //00005923 Executa el resultado y guarda los valores que la Query selecciono
+            while (rs.next()) { //00005923 Mientras haya un siguiente la ObservalbeList seguira guardando atributos
+                facilitatorIds.add(rs.getInt("id_facilitator")); //00005923 Agrega la ObservableList por medio de la columna
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage()); // Muestra alerta de error general  00005923
+        }
+        return facilitatorIds; //00005923 devuelve la ObservableListe para ser usada en el comboBox
     }
 
     @FXML
@@ -75,9 +99,9 @@ public class InsertsController implements Initializable { // Define la clase pri
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbBank", "root", "Egualos123"); // Conecta a la base de datos  00009423
             PreparedStatement st = conn.prepareStatement("INSERT INTO card (card_number, expiration_date, card_type, id_facilitator, id_client) VALUES (?, ?, ?, ?, ?)"); // Prepara la consulta SQL  00009423
             st.setString(1, txtCardNumber.getText()); // Establece el número de tarjeta  00009423
-            st.setDate(2, Date.valueOf(txtExpiration.getText())); // Establece la fecha de expiración  00009423
+            st.setDate(2, Date.valueOf(dpCard.getValue())); // Establece la fecha de expiración por medio del date picker 00005923
             st.setString(3, txtCardType.getText()); // Establece el tipo de tarjeta  00009423
-            st.setInt(4, cmbFacilitator.getSelectionModel().getSelectedIndex() + 1); // Establece el ID del facilitador  00009423
+            st.setInt(4, cmbFacilitator.getSelectionModel().getSelectedItem()); // Establece el ID del facilitador  00005923
             st.setInt(5, Integer.parseInt(txtIdClient.getText())); // Establece el ID del cliente  00009423
             try {
                 int result = st.executeUpdate(); // Ejecuta la consulta  00009423
@@ -128,7 +152,7 @@ public class InsertsController implements Initializable { // Define la clase pri
             st.setDouble(1, Double.parseDouble(txtMoneyAmount.getText())); // Establece la cantidad de dinero 00009423
             st.setString(2, txtDesc.getText()); // Establece la descripción 00009423
             st.setInt(3, Integer.parseInt(txtIdCard.getText())); // Establece el ID de la tarjeta 00009423
-            st.setDate(4, Date.valueOf(txtTransc.getText())); // Establece la fecha de la transacción 00009423
+            st.setDate(4, Date.valueOf(dpTransaction.getValue())); // Establece la fecha de la transacción por el datePicker 00005923
             try {
                 int result = st.executeUpdate(); // Ejecuta la consulta 00009423
                 showSuccesAlert("Added client successfully"); // Muestra alerta de éxito 00009423
@@ -179,9 +203,9 @@ public class InsertsController implements Initializable { // Define la clase pri
 
             st.setInt(1, Integer.parseInt(txtIdCard.getText())); // Establece el ID de la tarjeta 00009423
             st.setString(2, txtCardNumber.getText()); // Establece el número de tarjeta 00009423
-            st.setDate(3, Date.valueOf(txtExpiration.getText())); // Establece la fecha de expiración 00009423
+            st.setDate(3, Date.valueOf(dpCard.getValue())); // Establece la fecha de expiración por el datePicker 00005923
             st.setString(4, txtCardType.getText()); // Establece el tipo de tarjeta 00009423
-            st.setInt(5, cmbFacilitator.getSelectionModel().getSelectedIndex() + 1); // Establece el ID del facilitador 00009423
+            st.setInt(4, cmbFacilitator.getSelectionModel().getSelectedItem()); // Establece el ID del facilitador 00009423
             st.setInt(6, Integer.parseInt(txtIdClient.getText())); // Establece el ID del cliente 00009423
             try {
                 int result = st.executeUpdate(); // Ejecuta la consulta 00009423
@@ -209,7 +233,7 @@ public class InsertsController implements Initializable { // Define la clase pri
             st.setDouble(2, Double.parseDouble(txtMoneyAmount.getText())); // Establece la cantidad de dinero 00009423
             st.setString(3, txtDesc.getText()); // Establece la descripción 00009423
             st.setInt(4, Integer.parseInt(txtIdCard.getText())); // Establece el ID de la tarjeta 00009423
-            st.setDate(5, Date.valueOf(txtTransc.getText())); // Establece la fecha de la transacción 00009423
+            st.setDate(5, Date.valueOf(dpTransaction.getValue())); // Establece la fecha de la transacción por el datePicker 00005923
             try {
                 int result = st.executeUpdate(); // Ejecuta la consulta
                 showSuccesAlert("Updated transaction successfully"); // Muestra alerta de éxito 00009423
@@ -224,20 +248,69 @@ public class InsertsController implements Initializable { // Define la clase pri
 
     @FXML
     private void DeleteClient(ActionEvent event) { // Método para eliminar cliente
-        // Aquí va el código para eliminar el cliente
-        clearFields(); // Limpia los campos después de eliminar
+        if (txtIdClientDel.getText().isEmpty()) { //00005923 Evalua si es posible escribir en el textfield
+            showErrorAlert("Please enter the Client ID to delete"); //muestra un mensaje de error 00005923
+            return;
+        }
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbBank", "root", "Egualos123");
+            PreparedStatement st = conn.prepareStatement("DELETE FROM client WHERE id_client = ?");
+            st.setInt(1, Integer.parseInt(txtIdClientDel.getText()));
+            try {
+                int result = st.executeUpdate(); // Ejecuta la consulta 00005923
+                showSuccesAlert("Deleted Client successfully"); // Muestra alerta de éxito 00005923
+                clearFields(); // Limpia los campos de texto 00005923
+            } catch (SQLException e) {
+                showErrorAlert(e.getMessage()); // Muestra alerta de error SQL 00005923
+            }
+        }catch (Exception e){
+            showErrorAlert(e.getMessage());
+        }
     }
 
     @FXML
     private void DeleteCard(ActionEvent event) { // Método para eliminar tarjeta
-        // Aquí va el código para eliminar la tarjeta
-        clearFields(); // Limpia los campos después de eliminar
+        if (txtIdCardDel.getText().isEmpty()) { //00005923 Evalua si es posible escribir en el textfield
+            showErrorAlert("Please enter the Card ID to delete"); //muestra un mensaje de error 00005923
+            return;
+        }
+        try{
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbBank", "root", "Egualos123");
+            PreparedStatement st = conn.prepareStatement("DELETE FROM card WHERE id_card = ?");
+            st.setInt(1, Integer.parseInt(txtIdCardDel.getText()));
+            try {
+                int result = st.executeUpdate(); // Ejecuta la consulta 00005923
+                showSuccesAlert("Deleted Card successfully"); // Muestra alerta de éxito 00005923
+                clearFields(); // Limpia los campos de texto 00005923
+            } catch (SQLException e) {
+                showErrorAlert(e.getMessage()); // Muestra alerta de error SQL 00005923
+            }
+        }catch (Exception e){
+            showErrorAlert(e.getMessage());
+        }
+
     }
 
     @FXML
     private void DeleteTransaction(ActionEvent event) { // Método para eliminar transacción
-        // Aquí va el código para eliminar la transacción
-        clearFields(); // Limpia los campos después de eliminar
+        if (txtIdTransactionDel.getText().isEmpty()) { //00005923 Evalua si es posible escribir en el textfield
+            showErrorAlert("Please enter the Transaction ID to delete"); //muestra un mensaje de error
+            return;
+        }
+        try{
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbBank", "root", "Egualos123");
+            PreparedStatement st = conn.prepareStatement("DELETE FROM transaction WHERE id_transaction = ?");
+            try {
+                int result = st.executeUpdate(); // Ejecuta la consulta 00005923
+                showSuccesAlert("Deleted transaction successfully"); // Muestra alerta de éxito 00005923
+                clearFields(); // Limpia los campos de texto 00005923
+            } catch (SQLException e) {
+                showErrorAlert(e.getMessage()); // Muestra alerta de error SQL 00005923
+            }
+        }catch (Exception e){
+            showErrorAlert(e.getMessage());
+        }
+
     }
 
     private void showErrorAlert(String message) { // Método para mostrar alerta de error  00009423
@@ -271,5 +344,8 @@ public class InsertsController implements Initializable { // Define la clase pri
         txtIdCard.clear(); // Limpia el campo de ID de tarjeta 00009423
         txtTransc.clear(); // Limpia el campo de fecha de transacción 00009423
         txtIdTransaction.clear(); // Limpia el campo de ID de transacción 00009423
+        txtIdClientDel.clear(); //Lipia el campo de ID para borrar la Cliente 00005923
+        txtIdCardDel.clear(); //Lipia el campo de ID para borrar la Tarjeta 00005923
+        txtIdTransactionDel.clear(); //Lipia el campo de ID para borrar la transaccion 00005923
     }
 }
